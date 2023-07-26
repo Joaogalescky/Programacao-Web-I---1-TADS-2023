a4283a7f00f8b49547f501f70c676d9cef00e9e3
Joaogalescky <joaocvgalescky@gmail.com>
2023-07-26 20:01:37 -0300

    Re-upload alterações

--- tabuada.js
+++ tabuada.js
@@ -1,32 +1,37 @@
-//Obter os elementos da página
+//Obter elementos da página
 const frm = document.querySelector("form");
 const resp = document.querySelector("pre");
+const smpar = document.querySelector("#outPares");
+const smimpar = document.querySelector("#outImpares");
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    //obter o número informado
+    //Obter número informado
     const numero = Number(frm.inNumero.value);
-    
-    let resposta = "", r_impar = "", r_par = "";
-    let impares = 0, pares = 0;
 
-    //cria um laço de repetição iniciando de 1 até 10
-    //tabuada
-    for(let i = 1; i <= 10; i++){
+    let resposta = "", pares = 0, impares = 0;
+
+    //---------------------------------------------------------------    
+    //Tabuada
+    for(let i = 1; i <= 10; i++) { //criar um laço de repetição iniciando de 1 até 10
         resposta += numero + "X" + i + "=" + (numero*i) + "\n";
-        // Outra forma ---> resposta = `${resposta} ${numero} x ${i} = ${numero * i}\n`
-
-        //soma dos numeros impares e pares
-        if(i%2==1){ //impar
+        //resposta = `${resposta} ${numero} x ${i} = ${numero*i}\n`;
+    //---------------------------------------------------------------
+    //Par e impar
+        if(numero*i%2==0){ // verifca se é par
+            pares += numero*i;    
+        }else{ // verifica se é impar
             impares += numero*i;
-        }else{ //par
-            pares += numero*i;
         }
     }
 
-    resposta = respota + `A soma dos pares é: ${pares}` + "\n" + `À soma dos impares é: ${impares}`;
+    //Resposta = respota + `A soma dos pares é: ${pares}` + "\n" + `À soma dos impares é: ${impares}`;
+    //Resposta = resposta + `soma dos pares: ${somapares}\n`
+    //Resposta = resposta + `soma dos impares: ${somaimpares}\n`
 
-    //alterar o conteúdo da tag pre
+    //Vai alterar o conteúdo da tag pre
     resp.innerText = resposta;
+    smpar.innerText=somapares;
+    smimpar.innerText=somaimpares;
 })
