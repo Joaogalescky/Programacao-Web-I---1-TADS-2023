64c1871cc254aa538419296894ddf11f5a022c07
João Vitor Campõe Galescky <126702799+Joaogalescky@users.noreply.github.com>
2023-08-09 14:33:05 -0300

    Update consultorio.js

--- Exercícios em sala/consultorio.js
+++ Exercícios em sala/consultorio.js
@@ -5,6 +5,7 @@ https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects
 https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 */
 
+//const = cria uma váriavel com valor fixo, uma constante somente leitura
 
 const frm = document.querySelector("form");
 const respNome = document.querySelector("span");
