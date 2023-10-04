a22b3830c190f2028a0ca97501bb2bc40a4108f1
Joaogalescky <joaocvgalescky@gmail.com>
2023-10-04 16:15:38 -0300

    alterações - by folder of Emerson Bottega

--- Aula_12_09_2023/teatro.js
+++ Aula_12_09_2023/teatro.js
@@ -7,66 +7,64 @@ const POLTRONAS = 240; //quantidade de poltronas
 const reservadas = []; //vetor com as poltronas reservadas
 
 window.addEventListener("load", () => {
-    //criar "split" (separador de string)
-    const ocupadas = localStorage.getItem("teatroOcupadas")
-        ? localStorage.getItem("teatroOcupadas").split(";")
-        : [];
 
-    for(let i = 1; i <= POLTRONAS; i++){
+    //criar "split" (separador de string)
+    const ocupadas = localStorage.getItem("teatroOcupadas") ? localStorage.getItem("teatroOcupadas").split(";") : [];
+
+    for(let i = 1; i <= POLTRONAS; i++) {
+
         const figure = document.createElement("figure"); //tag figure
         const imgStatus = document.createElement("img"); //tag img
 
-        imgStatus.src = ocupadas.includes(i.toString())
-            ? "assets/ocupada.jpg"
-            : "assets/disponivel.jpg";
-        imgStatus.className = "poltrona"
+        imgStatus.src = ocupadas.includes(i.toString()) ? "assets/img/ocupada.jpg" : "assets/img/disponivel.jpg";
+        imgStatus.className = "poltrona";
 
         const figureCap = document.createElement("figcaption");
-        
-        //quantidade de zeros antes do número da poltrona
+
+        //quantidade de zeros antes do numero de poltronas
         const zeros = i < 10 ? "00": i < 100 ? "0" : "";
 
-        //criar número das poltronas
+        //cria o número da poltrona
         const num = document.createTextNode(`[${zeros}${i}]`);
 
         figureCap.appendChild(num);
         figure.appendChild(imgStatus);
         figure.appendChild(figureCap);
 
-        if(i % 24 ==12) figure.style.marginRight = "60px";
+        if(i % 24 == 12) figure.style.marginRight = "60px";
 
         dvPalco.appendChild(figure);
 
         if(i % 24 == 0 && dvPalco.appendChild(document.createElement("br")));
     }
-})
+});
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const poltrona = Number(frm.inPoltrona.value);
 
-    const ocupadas = localstorage.getItem("teatroOcupadas")
-    ? localStorage.getItem("teatroOcupadas").split(";")
+    const ocupadas = localStorage.getItem("teatroOcupadas") 
+    ? localStorage.getItem("teatroOcupadas").split(";") 
     : [];
 
-    if(poltrona > POLTRONAS){
-        alert("Informe o número de poltrona válido");
+    if (poltrona > POLTRONAS){
+        alert("Informe um número de poltrona válido");
         frm.inPoltrona.value = "";
         frm.inPoltrona.focus();
         return;
     }
 
     if(ocupadas.includes(poltrona.toString())){
-        alert(`Poltrona ${poltrona} já esta reservada`);
+        alert(`Poltrona ${poltrona} já está reservada`);
         frm.inPoltrona.value = "";
         frm.inPoltrona.focus();
         return;
     }
-
-    //captura a imagem da poltrona, filho de divPalco. É -1 pois começa em 0
+    
+    //captura a imagem da poltrona, filha de div palco. E -1 para começar em 0
     const imgPoltrona = dvPalco.querySelectorAll("img")[poltrona - 1];
-    imgPoltrona.src = "assets/reservada.jpg" //modifica o atributo da imagem
+    imgPoltrona.src = "assets/reservada.jpg";
     reservadas.push(poltrona);
 
     frm.inPoltrona.value = "";
@@ -76,21 +74,22 @@ frm.addEventListener("submit", (e) => {
 frm.btConfirmar.addEventListener("click", () => {
 
     if(reservadas.length == 0){
-        alert("Não há poltronas reservadas");
+        alert("Não há poltronas reservadas")
         frm.inPoltrona.focus();
         return;
     }
 
-    const ocupadas = localstorage.getItem("teatroOcupadas")
-    ? localStorage.getItem("teatroOcupadas").split(";")
+    const ocupadas = localStorage.getItem("teatroOcupadas") 
+    ? localStorage.getItem("teatroOcupadas").split(";") 
     : [];
     
     for(let i = reservadas.length - 1; i >= 0; i--){
         ocupadas.push(reservadas[i]);
         const imgPoltrona = dvPalco.querySelectorAll("img")[reservadas[i] -1];
-        imgPoltrona.src = "assets/ocupada.jpg"
+        imgPoltrona.src = "assets/ocupada.jpg";
 
         reservadas.pop();
     }
+
     localStorage.setItem("teatroOcupadas", ocupadas.join(";"));
 })
